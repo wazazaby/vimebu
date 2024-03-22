d4bc5fb257f26fa464b30c9944f134508fab3230
Teddy Sommavilla <teddy@batch.com>
2024-03-22 15:37:19 +0100

    refactor: len constraints are now vars
    That way, users of the libary can override them to fit their needs.

--- vimebu.go
+++ vimebu.go
@@ -9,11 +9,13 @@ import (
 	"unsafe"
 )
 
-const (
+var (
 	MetricNameMaxLen = 256  // MetricNameMaxLen is the maximum len in bytes allowed for the metric name.
 	LabelNameMaxLen  = 128  // LabelNameMaxLen is the maximum len in bytes allowed for a label name.
 	LabelValueLen    = 1024 // LabelValueLen is the maximum len in bytes allowed for a label value.
+)
 
+const (
 	leftBracketByte  = byte('{')
 	rightBracketByte = byte('}')
 	commaByte        = byte(',')
